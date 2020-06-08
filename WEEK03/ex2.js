16d132d966a21be04fd1648a9e56f2d917bb51f7
a8118045 <a8118045@aoyama.jp>
2020-06-09 01:16:02 +0900

    11-2

---
+++ WEEK03/ex2.js
@@ -0,0 +1,12 @@
+function buttonAPressed() {
+    var x = document.getElementById('textfield').innerHTML;
+    var html = ''
+    for (var i = 1; i <= x; i++) {
+        var y = 1
+        for (var j = 1; j <= i; j++) {
+            var y = y * j
+        }
+        html = html + '<li>' + i + 'の階乗は' + y + '</li>'
+    }
+    document.getElementById('num').innerHTML = html;
+}
